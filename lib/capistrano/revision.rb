30e61d0e3fd170cbc6df7ee9e79ece9309538e62
Yoshimoto <toshi@millefiori.co.jp>
2017-09-21 06:26:44 +0900

    revision 取得

--- lib/capistrano/revision.rb
+++ lib/capistrano/revision.rb
@@ -2,6 +2,37 @@ require "capistrano/revision/version"
 
 module Capistrano
   module Revision
-    # Your code goes here...
+    extend self
+
+    def latest?
+      current == deployed
+    end
+
+    def current
+      File.read('REVISION').chomp rescue nil
+    end
+
+    def deployed
+      if File.exist? revision_log
+        tail(revision_log) =~ %r"Branch .+ \(at (\w+)\)"
+        $1
+      end
+    end
+
+    def tail filename
+      File.open filename do |file|
+        file.seek -[line_length, File.size(filename)].min, IO::SEEK_END
+        body = file.read
+        body.lines.map(&:chomp).last
+      end
+    end
+
+    def revision_log
+      '../../revisions.log'
+    end
+
+    def line_length
+      70 + 32 + 40 + 14 + 16 # revision_log_message.size + branch + sha1 + release + user
+    end
   end
 end
--- spec/capistrano/revision_spec.rb
+++ spec/capistrano/revision_spec.rb
@@ -3,7 +3,74 @@ RSpec.describe Capistrano::Revision do
     expect(Capistrano::Revision::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe "#latest?" do
+    subject { described_class.latest? }
+    before do
+      allow(described_class).to receive(:current).and_return current
+      allow(described_class).to receive(:deployed).and_return deployed
+    end
+
+    context "on development" do
+      let(:current) { nil }
+      let(:deployed) { nil }
+
+      it { should be_truthy }
+    end
+
+    context "REVISION が無いとき" do
+      let(:current) { nil }
+      let(:deployed) { "a" }
+
+      it { should be_falsy }
+    end
+
+    context "revision.log が読めなかったとき" do
+      let(:current) { "a" }
+      let(:deployed) { nil }
+
+      it { should be_falsy }
+    end
+
+    context "一致する時" do
+      let(:current) { "a" }
+      let(:deployed) { "a" }
+
+      it { should be_truthy }
+    end
+
+    context "一致しない時" do
+      let(:current) { "a" }
+      let(:deployed) { "b" }
+
+      it { should be_falsy }
+    end
+  end
+
+  describe "#tail" do
+    subject { described_class.tail filename }
+
+    context "normal case" do
+      let(:filename) { "spec/fixtures/revisions.log" }
+
+      it { should == "Branch master (at 1ba4170be1c0f259fdb7c44dc92ffc9bd8fc05bf) deployed as release 20170122151156 by ubuntu" }
+    end
+
+    context "empty log" do
+      let(:filename) { "spec/fixtures/empty.log" }
+
+      it { should be_nil }
+    end
+
+    describe "one line" do
+      let(:filename) { "spec/fixtures/one_line.log" }
+
+      it { should == "Branch master (at 9dafdf7511417845d5619c5d961403fd2c8d6d08) deployed as release 20170122145018 by ubuntu" }
+    end
+
+    context "long branch name" do
+      let(:filename) { "spec/fixtures/long_branch.log" }
+
+      it { should == "Branch long-long-branch-name (at 1bea8232ffb57cf287dca033c53dccdf9e068993) deployed as release 20170206150230 by ubuntu" }
+    end
   end
 end
